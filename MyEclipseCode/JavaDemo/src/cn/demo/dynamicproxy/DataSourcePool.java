1ed457e0443169bb17e2a91a140dc9fbc9fbdb44
vincent <368255737@qq.com>
2018-06-01 18:07:07 +0800

    javademo-log4j dynamicproxy

---
+++ MyEclipseCode/JavaDemo/src/cn/demo/dynamicproxy/DataSourcePool.java
@@ -0,0 +1,489 @@
+package cn.demo.dynamicproxy;
+
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.sql.Array;
+import java.sql.Blob;
+import java.sql.CallableStatement;
+import java.sql.Clob;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.NClob;
+import java.sql.PreparedStatement;
+import java.sql.SQLClientInfoException;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.sql.SQLWarning;
+import java.sql.SQLXML;
+import java.sql.Savepoint;
+import java.sql.Statement;
+import java.sql.Struct;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.Executor;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+
+
+/*
+ * 自定义连接池
+ * 1、初始化10个Connection
+ * 2、为应用程序提供Connection
+ * 3、包装设计模式
+ */
+public class DataSourcePool implements DataSource  {
+
+	//集合保存Connection
+	private static LinkedList<Connection> list =  new LinkedList<Connection>();
+	
+	//初始化集合
+	static{
+		InputStream in = DataSourcePool.class.getClassLoader().getResourceAsStream("db.properties");
+		Properties prop = new Properties();
+		try{
+			prop.load(in);
+			
+			String driver = prop.getProperty("driver");
+			String url = prop.getProperty("url");
+			String user = prop.getProperty("user");
+			String password = prop.getProperty("password");
+			//注册驱动
+			Class.forName(driver);
+			//通过DriverManager获取一批Connection，存入集合
+			for(int i=0;i<10;i++){
+				Connection conn = DriverManager.getConnection(url, user, password);
+				//加入集合
+				list.add(conn);
+			}
+			
+		}catch (Exception e) {
+			throw new ExceptionInInitializerError(e);
+		}
+	}
+	
+	@Override
+	public PrintWriter getLogWriter() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setLogWriter(PrintWriter out) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setLoginTimeout(int seconds) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getLoginTimeout() throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T unwrap(Class<T> iface) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isWrapperFor(Class<?> iface) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Connection getConnection() throws SQLException {
+		// TODO Auto-generated method stub
+		if(list.size() > 0){
+			//还有连接
+			Connection conn = list.removeFirst();//mysql的一个实现
+			System.out.println( "获取连接:" + conn);
+			
+			MyConnection myconn = new MyConnection(conn,list);
+			
+			return myconn;
+		}else{
+			//没有连接
+			throw new RuntimeException("系统忙，请稍后.....");
+		}
+		
+	}
+
+	@Override
+	public Connection getConnection(String username, String password) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
+
+class MyConnection implements Connection{
+
+	//被增强对象的实例
+	private Connection conn = null;
+	private LinkedList<Connection> list = null;
+	
+	public MyConnection(Connection conn,LinkedList<Connection> list){
+		this.conn = conn;
+		this.list = list;
+	}
+
+	//需要重写该方法:将Connection还入集合
+	@Override
+	public void close() throws SQLException {
+		//自己逻辑
+		System.out.println("连接还池:" + conn);
+		list.add(conn);
+	}
+	
+	@Override
+	public void clearWarnings() throws SQLException {
+		conn.clearWarnings();		
+	}
+
+	@Override
+	public void commit() throws SQLException {
+		conn.commit();
+		
+	}
+
+	@Override
+	public Array createArrayOf(String typeName, Object[] elements)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return conn.createArrayOf(typeName, elements);
+	}
+
+	@Override
+	public Blob createBlob() throws SQLException {
+		// TODO Auto-generated method stub
+		return conn.createBlob();
+	}
+
+	@Override
+	public Clob createClob() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public NClob createNClob() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SQLXML createSQLXML() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Statement createStatement() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Statement createStatement(int resultSetType, int resultSetConcurrency)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Statement createStatement(int resultSetType,
+			int resultSetConcurrency, int resultSetHoldability)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Struct createStruct(String typeName, Object[] attributes)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean getAutoCommit() throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getCatalog() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Properties getClientInfo() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getClientInfo(String name) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getHoldability() throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public DatabaseMetaData getMetaData() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getTransactionIsolation() throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Map<String, Class<?>> getTypeMap() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SQLWarning getWarnings() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isClosed() throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isReadOnly() throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isValid(int timeout) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String nativeSQL(String sql) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CallableStatement prepareCall(String sql) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CallableStatement prepareCall(String sql, int resultSetType,
+			int resultSetConcurrency) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CallableStatement prepareCall(String sql, int resultSetType,
+			int resultSetConcurrency, int resultSetHoldability)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PreparedStatement prepareStatement(String sql) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PreparedStatement prepareStatement(String sql, int autoGeneratedKeys)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PreparedStatement prepareStatement(String sql, int[] columnIndexes)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PreparedStatement prepareStatement(String sql, String[] columnNames)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PreparedStatement prepareStatement(String sql, int resultSetType,
+			int resultSetConcurrency) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PreparedStatement prepareStatement(String sql, int resultSetType,
+			int resultSetConcurrency, int resultSetHoldability)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void releaseSavepoint(Savepoint savepoint) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rollback() throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rollback(Savepoint savepoint) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setAutoCommit(boolean autoCommit) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setCatalog(String catalog) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setClientInfo(Properties properties)
+			throws SQLClientInfoException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setClientInfo(String name, String value)
+			throws SQLClientInfoException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setHoldability(int holdability) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setReadOnly(boolean readOnly) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Savepoint setSavepoint() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Savepoint setSavepoint(String name) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setTransactionIsolation(int level) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setTypeMap(Map<String, Class<?>> map) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isWrapperFor(Class<?> iface) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public <T> T unwrap(Class<T> iface) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setSchema(String schema) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getSchema() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void abort(Executor executor) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setNetworkTimeout(Executor executor, int milliseconds) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getNetworkTimeout() throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
---
+++ MyEclipseCode/JavaDemo/src/cn/demo/log4j/readme.txt
@@ -0,0 +1,80 @@
+Log4j由三个重要的组成构成：日志记录器(Loggers)，输出端(Appenders)和日志格式化器(Layout)。
+1.Logger：控制要启用或禁用哪些日志记录语句，并对日志信息进行级别限制
+2.Appenders : 指定了日志将打印到控制台还是文件中
+3.Layout : 控制日志信息的显示格式
+
+A：off         最高等级，用于关闭所有日志记录。
+B：fatal       指出每个严重的错误事件将会导致应用程序的退出。
+C：error      指出虽然发生错误事件，但仍然不影响系统的继续运行。
+D：warm     表明会出现潜在的错误情形。
+E：info         一般和在粗粒度级别上，强调应用程序的运行全程。
+F：debug     一般用于细粒度级别上，对调试应用程序非常有帮助。
+G：all           最低等级，用于打开所有日志记录。
+5种常用级别，依次为DEBUG、INFO、WARN、ERROR和FATAL
+建议使用4个级别，优先级从高到低分别是:1.error 2.warn 3.info 4.debug
+
+
+Log4j几种常用的输出目的地:
+a：org.apache.log4j.ConsoleAppender：将日志信息输出到控制台。
+b：org.apache.log4j.FileAppender：将日志信息输出到一个文件。
+c：org.apache.log4j.DailyRollingFileAppender：将日志信息输出到一个日志文件，并且每天输出到一个新的日志文件。
+d：org.apache.log4j.RollingFileAppender：将日志信息输出到一个日志文件，并且指定文件的尺寸，当文件大小达到指定尺寸时，会自动把文件改名，同时产生一个新的文件。
+e：org.apache.log4j.WriteAppender：将日志信息以流格式发送到任意指定地方。
+f：org.apache.log4j.jdbc.JDBCAppender：通过JDBC把日志信息输出到数据库中。
+
+
+日志格式化器Layout有四种：
+1.HTMLLayout : 格式化日志输出为HTML表格形式如图：
+2.SimpleLayout : 以一种非常简单的方式格式化日志输出，它打印三项内容：级别-信息
+      例：INFO - info
+3.PatternLayout : 根据指定的转换模式格式化日志输出，或者如果没有指定任何转换模式，就使用默认的转化模式格式。
+4.TTCCLayout : 包含日志产生的时间、线程、类别等等信息
+
+
+Log4j的配置
+配置Log4j环境就是指配置root Logger，包括把Logger为哪个级别，为它增加哪些Appender,以及为这些Appender设置Layout，等等。因为所有其他的Logger都是root Logger的后代，所以它们都继承了root Logger的性质。这些可以通过设置系统属性的方法来隐式地完成，也可以在程序中调用XXXConfigurator.configure()方法来显式地完成。有以下几种方式来配置Log4j。
+A：配置放在文件里，通过环境变量传递文件名等信息，利用Log4j默认的初始化过程解析并配置。
+B：配置放在文件里，通过应用服务器配置传递文件甸等信息，利用一个特定的Servlet来完成配置。
+C：在程序中调用BasicConfigor.configure()方法。
+D：配置放在文件里，通过命令行PropertyConfigurator.configure(args[])解析log4j.properties文件并配置Log4j。
+
+下面对BasicConfigurator.configure()方法和PropertyConfigurator.config()方法分别进行介绍。
+BasicConfigurator.configure()方法：
+它使用简单的方法配置Log4j环境。这个方法完成的任务是：
+1：用默认的方式创建PatternLayout对象p:
+  PatternLayout p = new PatternLayout("%-4r[%t]%-5p%c%x-%m%n");
+2：用p创建ConsoleAppender对象a，目标是System.out，标准输出设备：
+ ConsoleAppender a = new CpnsoleAppender(p,ConsoleAppender.SYSTEM_OUT);
+3：为root Logger增加一个ConsoleAppender p;
+ rootLogger.addAppender(p);
+4：把rootLogger的log level设置为DUBUG级别;
+ rootLogger.setLevel(Level.DEBUG);
+
+PropertyConfigurator.configure()方法：
+当使用以下语句生成Logger对象时：
+static Logger logger = Logger.getLogger(mycalss.class);  
+如果没有调用BasicConfigurator.configure()，PropertyConfigurator.configure()
+或DOMConfigurator.configure()方法，
+Log4j会自动加载CLASSPATH下名为log4j.properties的配置文件。
+如果把此配置文件改为其他名字，例如my.properties,程序虽然仍能运行，
+但会报出不能正确初始化Log4j系统的提示。这时可以在程序中加上：
+PropertyConfigurator.configure("classes/my.properties");  问题即可解决。
+
+
+
+4、Log4j的log4j.properties配置详解
+  虽然可以不用配置文件，而在程序中实现配置，但这种方法在如今的系统开发中显然是不可取的，能采用配置文件的地方一定一定要用配置文件。
+  Log4j支持两 种格式的配置文件：XML格式和Java的property格式。
+详细配置见：配置详解.txt
+
+
+
+
+
+
+
+
+
+
+
+
